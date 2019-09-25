d88eb0a52ca65dbb814dfbcff3bdde768aad54f2
kylianvc <kylianvancanneyt@gmail.com>
2019-09-25 21:49:13 +0200

    update 2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,52 @@
 import '../styles/index.scss';
+import Axios from 'axios';
 
-console.log('webpack starterkit');
+// jquery
+// $.get("https://datatank.stad.gent/4/mobiliteit/bezettingparkingsrealtime.json",
+//     function (response) {
+        
+//     },
+//     "dataType"
+// );
+/*
+Axios.get("https://datatank.stad.gent/4/mobiliteit/bezettingparkingsrealtime.json")
+    .then(function (response){
+        //handle succes
+        console.log(response);
+    })
+    .catch(function (error){
+        //handle error
+        console.log(error);
+    })
+    .finally(function () {
+        // always executed
+      });
+*/
+// function herladen (){
+
+// }
+
+
+setInterval (herladen, 10000);
+
+function herladen()
+{
+    Axios.get("/public/fallback.json")
+        .then(function (response){
+        checkJson(response.data);
+      
+    });
+}
+
+function checkJson(data){
+
+}
+
+//   
+
+//   if (windowlocation != url){
+//       setItem
+//   } 
+
+//   );
+
